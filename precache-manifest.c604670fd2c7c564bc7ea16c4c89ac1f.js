584e24d436710fbcce30c7d4dc22614b26b3b543
Ravi Jayaraman <ravijayaraman13894@gmail.com>
2020-09-09 02:03:37 +1000

    Updates

---
+++ precache-manifest.c604670fd2c7c564bc7ea16c4c89ac1f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "212e21439873911eea7e143835cdc6dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0a72c523472065a8bec0",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2ca368928ce9c2855c31",
+    "url": "/static/js/2.fb5bd536.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb5bd536.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a72c523472065a8bec0",
+    "url": "/static/js/main.a0722060.chunk.js"
+  },
+  {
+    "revision": "35ec47dfee4c65c275be",
+    "url": "/static/js/runtime-main.c6e37e88.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
