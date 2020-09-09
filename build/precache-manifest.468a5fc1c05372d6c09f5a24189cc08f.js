e851c780e6ad084eca8972152d97449e15477df3
Ravi Jayaraman <ravijayaraman13894@gmail.com>
2020-09-09 21:03:17 +1000

    Added assets and sections to describe my work

---
+++ build/precache-manifest.468a5fc1c05372d6c09f5a24189cc08f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0eeaaed57680a61eae536001a7c1148b",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "c7553554565d5dcda1a8",
+    "url": "/portfolio/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7f7506d485d96d436dd5",
+    "url": "/portfolio/static/js/2.4cbe87e7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.4cbe87e7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7553554565d5dcda1a8",
+    "url": "/portfolio/static/js/main.7734d3ca.chunk.js"
+  },
+  {
+    "revision": "1ce76c02ee4034b846c7",
+    "url": "/portfolio/static/js/runtime-main.19a89414.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/portfolio/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Grommet, Nav, Footer, Grid, Anchor, Box, Text, Heading, Avatar, Card, CardHeader, CardBody, Image, Button } from 'grommet';
+import { Linkedin, Github } from 'grommet-icons';
+import avatar from './img/main-avatar.svg';
+import skills from './img/skills.png';
+import goals from './img/career-goals.png';
+import past_exp from './img/past-exp.png';
+import current_proj from './img/current-projects.png'
 
-function App() {
+const theme = {
+  global: {
+    font: {
+      family: 'Roboto',
+      size: '18px',
+      height: '20px',
+    },
+  },
+};
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Grommet theme={theme}>
+          <Nav direction="row" background="status-warning" pad="medium" justify={"between"}>
+              <Box direction="row" justify="start" align="center" gap="medium">
+                  <Image size="xlarge" src={avatar} />
+                  <Box direction={"column"} pad={"medium"} margin={"medium"}>
+                      <Heading>Ravi Jayaraman</Heading>
+                      <Text>I'm an aspiring web designer portraying my story through illustrations. Currently living and working in Sydney, Australia. Explore more about me through my website and get in touch with me</Text>
+                  </Box>
+              </Box>
+              <Box justify="end" direction="row">
+                  <Anchor icon={<Linkedin size='xlarge' />} href="https://www.linkedin.com/in/ravi-jayaraman-527b2183/" target="_blank" hoverIndicator color={"dark-1"}/>
+                  <Anchor icon={<Github size='xlarge' />} href="https://github.com/ravijayaraman" target="_blank" hoverIndicator color={"dark-1"}/>
+              </Box>
+          </Nav>
+          <Grid margin="20px">
+              <Box direction={"row-responsive"} height={"medium"} gap={"medium"} justify={"center"}>
+                  <Card background="light-1">
+                      <CardHeader pad="medium"><h2>Skills</h2></CardHeader>
+                      <CardBody pad="medium">
+                          <Image fit={"contain"} src={skills} />
+                      </CardBody>
+                  </Card>
+                  <Card background="light-1">
+                      <CardHeader pad="medium"><h2>Career Goals</h2></CardHeader>
+                      <CardBody pad="medium">
+                          <Image fit={"contain"} src={goals}/>
+                      </CardBody>
+                  </Card>
+                  <Card background="light-1">
+                      <CardHeader pad="medium"><h2>Past Experience</h2></CardHeader>
+                      <CardBody pad="medium">
+                          <Image fit={"contain"} src={past_exp}/>
+                      </CardBody>
+                  </Card>
+                  <Card background="light-1">
+                      <CardHeader pad="medium"><h2>Current Projects</h2></CardHeader>
+                      <CardBody pad="medium">
+                          <Image fit={"contain"} src={current_proj}/>
+                      </CardBody>
+                  </Card>
+              </Box>
+          </Grid>
+          <Footer background="status-warning" pad="small">
+              <h3>Copyright © Ravi Jayaraman</h3>
+          </Footer>
+      </Grommet>
+);
 }
 
 export default App;
